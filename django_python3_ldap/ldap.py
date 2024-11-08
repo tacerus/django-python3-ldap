98d1b7fd5d6ebeca10bc7aa6fb5d0f962d5efddc
Maffei Riccardo <RMaffei@Baumer.com>
2024-11-08 16:05:16 +0100

    Implemented functionalities to allow multiple LDAP configs

--- django_python3_ldap/ldap.py
+++ django_python3_ldap/ldap.py
@@ -259,6 +259,12 @@ def authenticate(request, *args, **kwargs):
     in settings.LDAP_AUTH_USER_LOOKUP_FIELDS, plus a `password` argument.
     """
     password = kwargs.pop("password", None)
+
+    if settings_prefix := kwargs.pop("prefix"):
+        settings.set_or_clear_prefix(settings_prefix)
+    else:
+        settings.set_or_clear_prefix()
+
     auth_user_lookup_fields = frozenset(settings.LDAP_AUTH_USER_LOOKUP_FIELDS)
     ldap_kwargs = {
         key: value for (key, value) in kwargs.items()
